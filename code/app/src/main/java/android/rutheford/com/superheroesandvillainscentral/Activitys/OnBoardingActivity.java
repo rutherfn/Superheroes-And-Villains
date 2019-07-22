220e6cbac7a6a9afb1a1f3e1ad21ae3724338281
Nicholas Rutherford <rutherfn@gmatc.matc.edu>
2019-07-22 14:28:06 -0500

    created layout for onboarding screens

---
+++ code/app/src/main/java/android/rutheford/com/superheroesandvillainscentral/Activitys/OnBoardingActivity.java
@@ -0,0 +1,15 @@
+package android.rutheford.com.superheroesandvillainscentral.Activitys;
+
+import android.rutheford.com.superheroesandvillainscentral.R;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class OnBoardingActivity extends AppCompatActivity
+{
+    @Override
+    public void setContentView(View view)
+    {
+        super.setContentView(view);
+        setContentView(R.layout.onboarding_main);
+    }
+}
